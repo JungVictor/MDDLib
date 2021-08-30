8e7e77b94c77cc20754e75d38598617e7e76525a
MalalelSteve <steve.malalel@etu.unice.fr>
2021-08-30 15:09:01 +0200

    Adding a new constraint builder for the multiplication of integer with relaxation, really

---
+++ dev/src/main/java/confidence/parameters/ParametersMulRelaxed.java
@@ -0,0 +1,101 @@
+package confidence.parameters;
+
+import memory.Allocable;
+import memory.AllocatorOf;
+import structures.arrays.ArrayOfDouble;
+
+public class ParametersMulRelaxed implements Allocable {
+
+    // Thread safe allocator
+    private final static ThreadLocal<Allocator> localStorage = ThreadLocal.withInitial(Allocator::new);
+    // Index in Memory
+    private final int allocatedIndex;
+
+
+    // References, must not be free or cleaned by the object
+    private double min, max;
+    private ArrayOfDouble vMin, vMax;
+    private double maxProbaDomains;
+    private double maxProbaEpsilon;
+
+    //**************************************//
+    //           INITIALISATION             //
+    //**************************************//
+
+    /**
+     * Get the allocator. Thread safe.
+     * @return The allocator.
+     */
+    private static Allocator allocator(){
+        return localStorage.get();
+    }
+
+    private ParametersMulRelaxed(int allocatedIndex){
+        this.allocatedIndex = allocatedIndex;
+    }
+
+    public void init(double min, double max, ArrayOfDouble vMin, ArrayOfDouble vMax, double maxProbaDomains, double maxProbaEpsilon){
+        this.min = min;
+        this.max = max;
+        this.vMin = vMin;
+        this.vMax = vMax;
+        this.maxProbaDomains = maxProbaDomains;
+        this.maxProbaEpsilon = maxProbaEpsilon;
+    }
+
+    public static ParametersMulRelaxed create(double min, double max, ArrayOfDouble vMin, ArrayOfDouble vMax, double maxProbaDomains, double maxProbaEpsilon){
+        ParametersMulRelaxed object = allocator().allocate();
+        object.init(min, max, vMin, vMax, maxProbaDomains, maxProbaEpsilon);
+        return object;
+    }
+
+    //**************************************//
+
+    public double min(){return min;}
+    public double max(){return max;}
+    public double vMin(int i){return vMin.get(i);}
+    public double vMax(int i){return vMax.get(i);}
+    public double maxProbaDomains(){return maxProbaDomains;}
+    public double maxProbaEpsilon(){return maxProbaEpsilon;}
+
+    //**************************************//
+    //           MEMORY FUNCTIONS           //
+    //**************************************//
+
+
+    @Override
+    public int allocatedIndex(){
+        return allocatedIndex;
+    }
+
+    @Override
+    public void free() {
+        allocator().free(this);
+    }
+
+    /**
+     * <b>The allocator that is in charge of the ParametersMulRelaxed type.</b><br>
+     * When not specified, the allocator has an initial capacity of 16. This number is arbitrary, and
+     * can be change if needed (might improve/decrease performance and/or memory usage).
+     */
+    static final class Allocator extends AllocatorOf<ParametersMulRelaxed> {
+
+        Allocator(int capacity) {
+            super.init(capacity);
+        }
+
+        Allocator(){
+            super.init();
+        }
+
+        @Override
+        protected ParametersMulRelaxed[] arrayCreation(int capacity) {
+            return new ParametersMulRelaxed[capacity];
+        }
+
+        @Override
+        protected ParametersMulRelaxed createObject(int index) {
+            return new ParametersMulRelaxed(index);
+        }
+    }
+}
---
+++ dev/src/main/java/confidence/states/StateMulRelaxed.java
@@ -0,0 +1,127 @@
+package confidence.states;
+
+import builder.constraints.states.NodeState;
+import confidence.parameters.ParametersMul;
+import confidence.parameters.ParametersMulRelaxed;
+import confidence.utils.SpecialOperations;
+import memory.AllocatorOf;
+
+import java.math.BigInteger;
+
+public strictfp class StateMulRelaxed extends NodeState {
+
+    // Thread safe allocator
+    private final static ThreadLocal<Allocator> localStorage = ThreadLocal.withInitial(Allocator::new);
+
+    private double mul;
+    private ParametersMulRelaxed constraint;
+
+    //**************************************//
+    //           INITIALISATION             //
+    //**************************************//
+
+    /**
+     * Get the allocator. Thread safe.
+     * @return The allocator.
+     */
+    private static Allocator allocator(){
+        return localStorage.get();
+    }
+
+    private StateMulRelaxed(int allocatedIndex) {
+        super(allocatedIndex);
+    }
+
+    public void init(ParametersMulRelaxed constraint){
+        this.constraint = constraint;
+        this.mul = constraint.maxProbaEpsilon();
+    }
+
+    public static StateMulRelaxed create(ParametersMulRelaxed constraint){
+        StateMulRelaxed object = allocator().allocate();
+        object.init(constraint);
+        return object;
+    }
+
+    public String toString(){
+        return Double.toString(mul);
+    }
+
+    //**************************************//
+
+
+    @Override
+    public NodeState createState(int label, int layer, int size) {
+        StateMulRelaxed state = StateMulRelaxed.create(constraint);
+        state.mul = SpecialOperations.multiplyCeil(mul, label, constraint.maxProbaDomains());
+        return state;
+    }
+
+    @Override
+    public boolean isValid(int label, int layer, int size){
+
+        double newMul = SpecialOperations.multiplyCeil(mul, label, constraint.maxProbaDomains());
+
+        //Lignes à revoir pour l'ordre dans lequel les multiplications sont faites
+        double minPotential = SpecialOperations.multiplyCeil(newMul, constraint.vMin(layer-1), constraint.maxProbaDomains());
+        double maxPotential = SpecialOperations.multiplyCeil(newMul, constraint.vMax(layer-1), constraint.maxProbaDomains());
+
+        //Si l'intervalle [minPotential, maxPotential] intersection [constraint.min(), constraint.max] est vide
+        if(maxPotential < constraint.min() || constraint.max() < minPotential ) return false;
+        //Si l'intervalle [minPotential, maxPotential] est inclus dans l'intervalle [constraint.min(), constraint.max]
+        if(constraint.min() <= minPotential && maxPotential <= constraint.max()) return true;
+
+        return newMul <= constraint.max();
+    }
+
+    @Override
+    public String hash(int label, int layer, int size){
+        double newMul = SpecialOperations.multiplyCeil(mul, label, constraint.maxProbaDomains());
+
+        double minPotential = SpecialOperations.multiplyCeil(newMul, constraint.vMin(layer-1), constraint.maxProbaDomains());
+        double maxPotential = SpecialOperations.multiplyCeil(newMul, constraint.vMax(layer-1), constraint.maxProbaDomains());
+
+        if(constraint.min() <= minPotential && maxPotential <= constraint.max()) return "";
+        return Double.toString(newMul);
+    }
+
+    @Override
+    public NodeState merge(NodeState state, int label, int layer, int size){
+        StateMulRelaxed stateMulrelaxed = (StateMulRelaxed) state;
+        double m = SpecialOperations.multiplyCeil(stateMulrelaxed.mul, label, constraint.maxProbaDomains());
+        if(m < mul) mul = m;
+        return null;
+    }
+
+    @Override
+    public void free(){
+        this.constraint = null;
+        allocator().free(this);
+    }
+
+    /**
+     * <b>The allocator that is in charge of the StateMulRelaxed type.</b><br>
+     * When not specified, the allocator has an initial capacity of 16. This number is arbitrary, and
+     * can be change if needed (might improve/decrease performance and/or memory usage).
+     */
+    static final class Allocator extends AllocatorOf<StateMulRelaxed> {
+
+        Allocator(int capacity) {
+            super.init(capacity);
+        }
+
+        Allocator(){
+            super.init();
+        }
+
+        @Override
+        protected StateMulRelaxed[] arrayCreation(int capacity) {
+            return new StateMulRelaxed[capacity];
+        }
+
+        @Override
+        protected StateMulRelaxed createObject(int index) {
+            return new StateMulRelaxed(index);
+        }
+    }
+}
