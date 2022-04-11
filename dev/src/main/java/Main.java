ea74bb2fd6c2b9c389d1987a91c1fb2fad5e48a7
Malalel <steve.malalel@etu.unice.fr>
2022-04-11 16:50:05 +0200

    Clear main, really

--- dev/src/main/java/Main.java
+++ dev/src/main/java/Main.java
@@ -1,8 +1,3 @@
-import csp.constraints.IntervalConstraintEqual;
-import csp.constraints.IntervalConstraintSum;
-import csp.IntervalConstraintsNetwork;
-import csp.IntervalVariable;
-
 public class Main {
 
     public static void main(String[] args) {
